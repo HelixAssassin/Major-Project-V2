a42c41da5f272392d0ca3fed943bb963fbbad2c8
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2020-05-20 20:43:32 +0100

    Preliminary Setup
    Because much of my previous Unity Major Project was unmessy due to rush and becasue I was also encountering a number of issues with my original Unity project such as random crashes during tests I decided to start fresh in a new Project folder while maintaining the original assets and objects that I was using in the original Major Project.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //This script controls the basic movement of the player, i.e. moving left and right and jumping
+    CharacterController charController;
+    [SerializeField] float jumpSpeed = 20.0f;
+    //This line controls the amount of time before the player is pulled back to the ground after jumping
+    [SerializeField] float gravity = 1.0f;
+    float yVelocity = 0.0f;
+
+    //This line controls the basic movement speed of the player's character
+    [SerializeField] float moveSpeed = 5.0f;
+    public float h;
+    public float v;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        // These two lines control the player and enable them to move both horizontal (left and right),
+        // and vertical (jumping up)
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        // These two lines control the speed (v = vertical), as well as the direction (h = horizontal),
+        // within animations.
+
+
+        // These lines control the direction of movement as well as the speed of movement.
+        Vector3 direction = new Vector3(h, 0, v);
+        Vector3 velocity = direction * moveSpeed;
+        // If the player is on the ground and presses the space bar, the player will jump (with animations).
+        if (charController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = jumpSpeed;
+            }
+
+        }
+        else
+        {
+            yVelocity -= gravity;
+        }
+        velocity.y = yVelocity;
+
+        velocity = transform.TransformDirection(velocity);
+
+        charController.Move(velocity * Time.deltaTime);
+
+    }
+}
