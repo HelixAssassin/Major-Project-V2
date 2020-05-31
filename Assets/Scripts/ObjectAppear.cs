0deebd26e6365abda47591bc4574de0851e782c3
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2020-05-31 16:04:59 +0100

    UI design
    I have begun crreating my preliminary UI for the scene. The UI in this scene is meant to show off environmental storytelling by having UI test displayed over environmental objects like the dead bodies and the terrain objects. This is the method in which I will be telling my environmental storytelling instead of the 3D world text objects used in my original project.

---
+++ Assets/Scripts/ObjectAppear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectAppear : MonoBehaviour
+{
+    void Start()
+    {
+
+        gameObject.GetComponent<Text>().enabled = false;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        gameObject.GetComponent<Text>().enabled = true;
+
+    }
+}
