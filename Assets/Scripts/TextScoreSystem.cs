c0ade5519f1b59a4693dc3e7f7df8453c36a11ee
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2020-06-09 18:03:40 +0100

    UI finishes and Menu work.
    I've made the final adjustments to my UI test placements and how large they are meant to bo. The only thing I have left to do for the UI is make a count system so that the player is able to know how many they have found. I have also reworked certain elements for my Main Menu. I want to be able to show a small 3D environment so that the menu doesn't feel too empty.

---
+++ Assets/Scripts/TextScoreSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextScoreSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theCount;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theCount += 1;
+        scoreText.GetComponent<Text>().text = "0/! " + theCount;
+    }
+}
