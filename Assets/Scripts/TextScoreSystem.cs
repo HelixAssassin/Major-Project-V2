4b24c6e7f0e29d18ddbd906d5839f35ab128294e
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2020-06-11 17:38:24 +0100

    Additional UI work
    I've made more progress on integrating the scroing system for the UI texts. I am also working on a pop-up panle for the pkayer to know what they are doing. I will have a Ui text asking players to look for a way into the castle and then if they need help on what to do or if they are confused there will be a tip icon which tells the players what optional UI texts they can find.

--- Assets/Scripts/TextScoreSystem.cs
+++ Assets/Scripts/TextScoreSystem.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 public class TextScoreSystem : MonoBehaviour
 {
-    public GameObject scoreText;
+    public GameObject countText;
     public int theCount;
 
     void OnTriggerEnter(Collider other)
     {
         theCount += 1;
-        scoreText.GetComponent<Text>().text = "0/! " + theCount;
+        countText.GetComponent<Text>().text = "THOUGHTS! " + theCount;
     }
 }
