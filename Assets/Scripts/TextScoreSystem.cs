4ee4654249ea164a3a0dce2fb2bf536378174ebb
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2020-06-13 18:07:09 +0100

    Scoring
    I am implementing a system where the number of thoughts encountered will be counted so that the player will know how many they have found comapred to how many there are. There are still a few bugs to work out but I will get back to it on my next session.

--- Assets/Scripts/TextScoreSystem.cs
+++ Assets/Scripts/TextScoreSystem.cs
@@ -5,11 +5,10 @@ using UnityEngine.UI;
 public class TextScoreSystem : MonoBehaviour
 {
     public GameObject countText;
-    public int theCount;
+    public static int theCount;
 
     void OnTriggerEnter(Collider other)
     {
-        theCount += 1;
-        countText.GetComponent<Text>().text = "THOUGHTS! " + theCount;
+        countText.GetComponent<Text>().text = "THOUGHTS: " + theCount;
     }
 }
