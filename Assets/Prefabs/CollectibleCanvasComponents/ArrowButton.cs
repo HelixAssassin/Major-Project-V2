f090df93a67ec0f172470e0ebddfed53fdce5945
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2020-06-24 15:06:20 +0100

    Near Final Collectilbe UI touches
    After some lengthy design problmes that took longer to fix then I wanted I have managed to reimplement the UI to allow the player to open and close the panels, collect the notes in any order. The last Issue I have left to fix is making the pop-up buttons non-interactable until the note has been collected.

---
+++ Assets/Prefabs/CollectibleCanvasComponents/ArrowButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowButton : MonoBehaviour
+{
+    private Inventory inventory;
+    public Button arrowButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        inventory.enabled = true; 
+        arrowButton.interactable = true;
+    }
+
+    private void FixedUpdate()
+    {
+        inventory.enabled = false;
+        arrowButton.interactable = false;
+    }
+}
